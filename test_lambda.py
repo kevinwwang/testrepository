f28f0c261b5bd50e6c9643b8270c09cf1b8013f7
kevinwwang <34076557+kevinwwang@users.noreply.github.com>
2017-12-21 14:33:28 -0600

    Update test_lambda.py

--- test_lambda.py
+++ test_lambda.py
@@ -5,7 +5,7 @@ def lambda_handler(event, context):
     # TODO implement
     dynamodb = boto3.resource("dynamodb")
     table = dynamodb.Table("DoesNotExist")
-    table.put_item(event)
+    table.put_item(Item=event)
     return {
         "version":"second version"
     }
