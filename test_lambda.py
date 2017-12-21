5f7453a5e26fcacf118153355822856a4c344a07
kevinwwang <34076557+kevinwwang@users.noreply.github.com>
2017-12-21 12:17:50 -0600

    Update test_lambda.py

--- test_lambda.py
+++ test_lambda.py
@@ -3,6 +3,8 @@ import os
 
 def lambda_handler(event, context):
     # TODO implement
+    dynamodb = boto3.resource("dynamodb")
+    table = dynamodb.Table(os.environ['USER_TABLE'])
     return {
         "version":"second version"
     }
